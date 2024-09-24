2fba31582fab3589598c7002f9e89fa5fe43e133
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-09-24 07:38:20 -0500

    View data

--- MVCDependencyInjectionType/Controllers/HomeController.cs
+++ MVCDependencyInjectionType/Controllers/HomeController.cs
@@ -10,11 +10,26 @@ namespace MVCDependencyInjectionType.Controllers
         private readonly IScoped _scoped;
         private readonly ISingleton _singleton;
 
-        public HomeController(ITrasient trasient, IScoped scoped, ISingleton singleton)
+        private readonly ITrasient _trasient2;
+        private readonly IScoped _scoped2;
+        private readonly ISingleton _singleton2;
+
+        public HomeController(
+            ITrasient trasient, 
+            IScoped scoped, 
+            ISingleton singleton,
+            ITrasient trasient2,
+            IScoped scoped2,
+            ISingleton singleton2
+            )
         {
             _trasient = trasient;
             _scoped = scoped;
             _singleton = singleton;
+
+            _trasient2 = trasient2;
+            _scoped2 = scoped2;
+            _singleton2 = singleton2;
         }
 
         public IActionResult Index()
@@ -22,6 +37,10 @@ namespace MVCDependencyInjectionType.Controllers
             ViewBag.trasient = _trasient;
             ViewBag.scoped = _scoped;
             ViewBag.singleton = _singleton;
+
+            ViewBag.trasient2 = _trasient2;
+            ViewBag.scoped2 = _scoped2;
+            ViewBag.singleton2 = _singleton2;
 
 
             return View();
