7a837e18d39d856daffebc1c0eee1f19e8156521
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-09-24 07:07:46 -0500

    Add MyObject class

---
+++ MVCDependencyInjectionType/MyObject.cs
@@ -0,0 +1,17 @@
+﻿namespace MVCDependencyInjectionType
+{
+    public interface ITrasient { };
+    public interface IScoped { };
+    public interface ISingleton { };
+
+
+    public class MyObject : ITrasient, IScoped, ISingleton 
+    {
+        public readonly int Value;
+
+        public MyObject()
+        {
+                Value = new Random().Next(10000);
+        }
+    }
+}
